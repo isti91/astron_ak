004df5b8139756d2c0839ef681e23c226b32fbd1
Somogyi István <istvan.somogyi@partner.astron.hu>
2023-06-22 02:11:03 +0200

    Creat Angular App

---
+++ frontend/src/app/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'astron-ak-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+	headerTitle: string = 'Astron Gépjármű Kölcsönző';
+}
