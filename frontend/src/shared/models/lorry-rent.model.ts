0864eb8696edc5f83384dcc6822d28284a963acf
Kozma Dávid Dániel <kozma@astron.hu>
2023-07-12 10:57:32 +0200

    jav

--- frontend/src/shared/models/lorry-rent.model.ts
+++ frontend/src/shared/models/lorry-rent.model.ts
@@ -1,9 +1,7 @@
 export interface LorryRentModel {
-    id: number; //egyedi id
-    licenseNumber: string; //rendszám
-    carType: string; //típusa
-    cargoVolume: number; //raktér térfogat
-    technicalValidity: Date; //Műszaki érvényessége, formátum: YYYY/MM/DD
-
-  }
-  
+  id: number; //egyedi id
+  licenseNumber: string; //rendszám
+  carType: string; //típusa
+  cargoVolume: number; //raktér térfogat
+  technicalValidity: Date; //Műszaki érvényessége, formátum: YYYY/MM/DD
+}
